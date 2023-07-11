08190db0da47c2f364a8e49a25ff49743b3a312a
wayan surya <119721384+uyaaaa@users.noreply.github.com>
2023-07-11 20:46:39 +0700

    penambahan route login dan kawan kawan

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DaftarController;
+use App\Http\Controllers\LupaPassController;
+use App\Http\Controllers\DashboardController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,85 +19,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('Backup');
-// });
-
-use App\Http\Controllers\BackupController;
-use App\Http\Controllers\General;
-use App\Http\Controllers\GeneralController;
-
 Route::get('/', function () {
-    return view('Beranda', [
-        "tittle" => "Halaman Utama"
-    ]);
+    return view('welcome');
 });
-
-Route::get('/Sekilas', function () {
-    return view('Sekilas',[
-        "tittle" => "Sekilas"
-    ]);
+Route::group(['middleware' => 'web'], function () {
+    // Rute-rute yang memerlukan akses session di sini
+    Route::get('/dashboardm', [DashboardController::class, 'index'])->name('dashboard-mahasiswa');
+// Tambahkan rute lain yang perlu diotentikasi di sini
+Route::get('/setting/backup', function () {
+    return view('backup');
 });
-
-Route::get('/Fitur', function () {
-    return view('Fitur', [
-        "tittle" => "Fitur Online"
-    ]);
+Route::get('/setting/version', function () {
+    return view('version');
 });
-
-Route::get('/Fasilitas', function () {
-    return view('Fasilitas',[
-        "tittle" => "Fasilitas Offline"
-    ]);
+Route::get('/setting/hapusakun', function () {
+    return view('hapusakun');
 });
-
-Route::get('/News', function () {
-    return view('News', [
-        "tittle" => "Berita Kami"
-    ]);
+Route::get('/setting', function () {
+    return view('settinguser');
 });
-
-Route::get('/InfoPMB', function () {
-    return view('InfoPMB', [
-        "tittle" => "Informasi Penerimaan Mahasiswa Baru"
-    ]);
 });
+Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
 
-Route::get('/Bantuan', function () {
-    return view('Bantuan', [
-        "tittle" => "Bantuan"
-    ]);
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+
+Route::get('/lupapass', function () {
+    return view('lupapass');
 });
+Route::post('/lupapass', [LupaPassController::class, 'update'])->name('lupapass.update');
 
-Route::get('/Kontak', function () {
-    return view('Kontak', [
-        "tittle" => "Kontak"
-    ]);
-});
+Route::get('/signup', [DaftarController::class, 'create'])->name('signup.create');
+Route::post('/signup', [DaftarController::class, 'store'])->name('signup.store');
 
-Route::get('/Maps', function () {
-    return view('Maps', [
-        "tittle" => "Lokasi Perpustakaan Amikom"
-    ]);
-});
-
-Route::get('/PanduanMasuk', function () {
-    return view('PanduanMasuk', [
-        "tittle" => "Panduan Masuk Perpustakaan Amikom"
-    ]);
-});
-
-
-Route::get('/Backup', [Backup::class, 'index'])->name('Backup.index');
-Route::get('/General', [General::class, 'general'])->name('General.general');
-// routes/web.php
-
-Route::get('/Backup', [BackupController::class, 'backup'])->name('backup.backup');
-
-Route::get('/Backup', function () {
-    return view('Backup');
-});
-
-
-
-
+Route::get('/signdosen', [DosenController::class, 'create'])->name('signdosen.create');
+Route::post('/signdosen', [DosenController::class, 'store'])->name('signdosen.store');
